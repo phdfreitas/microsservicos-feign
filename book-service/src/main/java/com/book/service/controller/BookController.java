9b4c9c636b4d98a17066279070a30c911ac1242a
phdfreitas <pedro.hnd.freitas@gmail.com>
2023-01-06 19:14:47 -0300

    Consumindo o Cambio Service a partir do Book Service usando Feign

--- book-service/src/main/java/com/book/service/controller/BookController.java
+++ book-service/src/main/java/com/book/service/controller/BookController.java
@@ -1,13 +1,18 @@
 package com.book.service.controller;
 
 import com.book.service.model.Book;
+import com.book.service.proxy.CambioProxy;
 import com.book.service.repository.BookRepository;
+import com.book.service.response.Cambio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -19,14 +24,47 @@ public class BookController {
     @Autowired
     private BookRepository repository;
 
+    @Autowired
+    private CambioProxy proxy;
+
+    @GetMapping("/{id}/{currency}")
+    public Book findBook(@PathVariable Long id, @PathVariable String currency){
+
+        var book = repository.findById(id).get();
+        var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
+
+        var port = environment.getProperty("local.server.port");
+        book.setEnvironment(port + "FEIGN");
+        book.setCurrency(currency);
+        book.setPrice(cambio.getConvertedValue());
+        return book;
+    }
+
+    /*
+      =-=-= Versão usando Rest Template =-=-=
     @GetMapping("/{id}/{currency}")
     public Book findBook(@PathVariable Long id, @PathVariable String currency){
 
         var book = repository.findById(id).get();
 
+        HashMap<String, String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+        var response = new RestTemplate()
+                .getForEntity(
+                        "http://localhost:8000/cambio-service/{amount}/{from}/{to}",
+                        Cambio.class,
+                        params);
+
+        var cambio = response.getBody();
+
         var port = environment.getProperty("local.server.port");
         book.setEnvironment(port);
+        book.setCurrency(currency);
+        book.setPrice(cambio.getConvertedValue());
         return book;
-    }
+    }*/
 
 }
---
+++ book-service/src/main/java/com/book/service/proxy/CambioProxy.java
@@ -0,0 +1,20 @@
+package com.book.service.proxy;
+
+import com.book.service.response.Cambio;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+
+@FeignClient(name = "cambio-service", url = "localhost:8000")
+public interface CambioProxy {
+
+    @GetMapping(value = "/cambio-service/{amount}/{from}/{to}")
+    Cambio getCambio(
+            @PathVariable("amount") BigDecimal amount,
+            @PathVariable("from") String from,
+            @PathVariable("to") String to
+    );
+
+}
