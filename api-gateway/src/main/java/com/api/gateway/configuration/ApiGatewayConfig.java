00fe108fd5118d9a35d3f2e8281aa49d57011310
phdfreitas <pedro.hnd.freitas@gmail.com>
2023-01-09 10:02:22 -0300

    Acessando endpoints a partir do api gateway

---
+++ api-gateway/src/main/java/com/api/gateway/configuration/ApiGatewayConfig.java
@@ -0,0 +1,29 @@
+package com.api.gateway.configuration;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class ApiGatewayConfig {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+
+        return builder.routes()
+                .route(predicate -> predicate.path("/get")
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/cambio-service/**")
+                        .uri("lb://cambio-service"))
+                .route(p -> p.path("/book-service/**")
+                        .uri("lb://book-service"))
+                .build();
+    }
+
+}
